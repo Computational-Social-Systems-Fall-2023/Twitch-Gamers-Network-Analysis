6f0c4df62cbf9d5a050ed36d379f4b4cfbe28ed7
tminhat13 <83794482+tminhat13@users.noreply.github.com>
2023-10-29 20:33:34 -0700

    init

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#############################################################
+# Twitch Gamers Network Analysis
+# Author: Nhat Tran
+#
+#############################################################
 import networkx as nx
 import random
 import pandas as pd
@@ -11,8 +12,6 @@ edges_df = pd.read_csv('large_twitch_edges.csv')
 
 # Create a graph from the edges
 g = nx.from_pandas_edgelist(edges_df, source='numeric_id_1', target='numeric_id_2')
-# g = nx.read_edgelist(Data, create_using=nx.DiGraph(), delimiter=',', nodetype=None, data=True, edgetype=None,
-#                      encoding='utf-8')
 
 # Identify connected components in the graph
 connected_components = list(nx.connected_components(g))
@@ -24,6 +23,7 @@ largest_component = max(connected_components, key=len)
 largest_subgraph = g.subgraph(largest_component)
 
 
+# define Watts Strogatz graph
 def create_watts_strogatz_graph(num_nodes, mean_degree, probability):
     graph = nx.Graph()
 
@@ -45,15 +45,19 @@ def create_watts_strogatz_graph(num_nodes, mean_degree, probability):
     return graph
 
 
-# Parameters
-n = largest_subgraph.order()  # Number of nodes
-k = largest_subgraph.size()  # Each node is connected to k nearest neighbors
-p = 0.2  # Probability of rewiring each edge
+# Number of nodes
+n = largest_subgraph.order()
+
+# Each node is connected to k nearest neighbors
+k = largest_subgraph.size()
+
+# Probability of rewiring each edge
+p = 0.2
 
 # Calculating Original Network size, degree, average path length, and clustering
 print("Nodes: ", n)
 print("Edges (Size): ", k)
-print("Average degree: ", float(k) / n)
+print("Average degree: ", float(k * 2) / n)
 print("Average clustering: ", nx.average_clustering(largest_subgraph))
 print("Average path length: ", nx.average_shortest_path_length(largest_subgraph))
 
@@ -66,6 +70,55 @@ print("Average clustering: ", nx.average_clustering(watts_strogatz_graph))
 print("Average path length: ", nx.average_shortest_path_length(watts_strogatz_graph))
 
 
+# define Barabasi-Albert method
+def create_barabasi_albert_graph(initial_number_of_nodes, number_of_expected_connections, time_to_run):
+    # check if m <= m0
+    if initial_number_of_nodes < number_of_expected_connections:
+        return 0
+
+    # Initial graph with m0 nodes and degrees of at least 1
+    graph = nx.complete_graph(initial_number_of_nodes)
+
+    if graph.degree() < 1:
+        return 0
+
+    for time in range(time_to_run):
+        # Add new node
+        new_node = graph.number_of_nodes()
+        graph.add_node(new_node)
+
+        # Connect the new node to a random node of the existing nodes
+        while graph.degree(new_node) < number_of_expected_connections:
+            # calculate probability
+            sum_degree = sum(graph.degree(v) for v in graph)
+            probability = [graph.degree(v) / sum_degree for v in graph.nodes()]
+            # select a random node
+            list_of_nodes = list(graph.nodes())
+            random_node = random.choices(list_of_nodes, weights=probability, k=1)[0]
+            # connect vj to a random node
+            graph.add_edge(new_node, random_node)
+    return graph
+
+
+# Initial number of nodes
+m0 = largest_subgraph.order()
+
+# Number of expected connections
+m = largest_subgraph.size()
+
+# Time to run
+t = 1000
+
+# Generate a scale-free network using Preferential Attachment
+barabasi_albert_graph = create_barabasi_albert_graph(m0, m, t)
+
+# Calculating average path length, and clustering of the watts_strogatz graph
+print("Barabasi Albert Graph")
+print("Average clustering: ", nx.average_clustering(barabasi_albert_graph))
+print("Average path length: ", nx.average_shortest_path_length(barabasi_albert_graph))
+
+
+# ===============================================================================================================
 # Visualize the network
 # pos = nx.circular_layout(watts_strogatz_graph)  # Circular layout for visualization
 # nx.draw(watts_strogatz_graph, pos, with_labels=True, node_size=300, node_color='skyblue')
